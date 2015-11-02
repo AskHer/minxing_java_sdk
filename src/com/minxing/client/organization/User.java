1f6b68256656cf0331fd8a0d871036c1631bedf2
yinwei <yinwei@dehuinet.com>
2015-11-02 18:38:27 +0800

    改user的toString

--- src/com/minxing/client/organization/User.java
+++ src/com/minxing/client/organization/User.java
@@ -334,7 +334,7 @@ public class User extends Organization {
 
 	@Override
 	public String toString() {
-		return "User<id:" + this.id + ",name:" + this.name + ",login_name:"
+		return "User<id:" + this.id + ",name:" + this.name + ",login_name:"+this.login_name 
 				+ ",email:" + this.email
 				+ ",cellvoice1:" + this.cellvoice1
 				+ ",emp_code:" + this.emp_code
@@ -342,7 +342,7 @@ public class User extends Organization {
 				+ ",network_id:" + this.network_id
 				+ ",role_code:" + this.role_code
 				+ ",avatar_url:" + this.avatar_url
-				+ this.login_name + ">";
+				+ ">";
 	}
 
 	public String getAvatarUrl() {
