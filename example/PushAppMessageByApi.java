a8d309426bb7f1970dbdb267d5ab8f983b8ef841
yinwei <yinwei@dehuinet.com>
2016-05-16 15:33:45 +0800

    提交

--- example/PushAppMessageByApi.java
+++ example/PushAppMessageByApi.java
@@ -8,7 +8,7 @@ public class PushAppMessageByApi {
 
 
 
-		AppAccount account = AppAccount.loginByAccessToken("http://127.0.0.1:3000", "iPefUDrrardwZMWQXaZnBDBCLyY3iksJTmYtP2rcrJ0EYCJA");
+		AppAccount account = AppAccount.loginByAccessToken("http://test.dehuinet.com:8030", "4jszLAnk3O0sjwO88043NmALCxFCEfLQVulrFqKx03czHfz1");
 		
 		
 		testPushAppMessage(account);
@@ -20,7 +20,7 @@ public class PushAppMessageByApi {
 		
 		AppMessage appMsg = new AppMessage(1,"test from message","{\"param1\": 1,\"param2\": 2}");
 		try {
-			int mid = account.pushAppMessage("ddod", "oajcs3@js.chinamobile.com",appMsg );
+			int mid = account.pushAppMessage("hot", "t66",appMsg );
 			System.out.println("message send out with id:" + mid);
 		} catch (ApiErrorException e) {
 			e.printStackTrace();
