66ce4392b1ac79c5b220ade488de5f54213263f1
quhb <13821056120@139.com>
2017-11-09 16:32:48 +0800

    容错

--- example/Test.java
+++ example/Test.java
@@ -1,24 +1,21 @@
-
-
-import java.util.List;
-
-import com.alibaba.fastjson.JSON;
 import com.minxing.client.app.AppAccount;
 import com.minxing.client.organization.User;
 
+
 public class Test {
-	
-	public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
 //		String url = "http://192.168.100.245";
 //		String token = "XrhAoAjWyWktkeLvVqAi8lf9z444mYgRxImwOx8K8gphnBbm";
-		String url = "http://test.dehuinet.com:8030";
-		String token = "lf-gJ4ATa8mkAwTTMvq8mV_5aD826q-Qr5AjhSkuoRZhp-k1";
-		AppAccount account = AppAccount.loginByAccessToken(url,token);
+        String url = "http://dev3.dehuinet.com:8013";
+        String token = "NdLtIi9QpWY7ZGhf4cOpVmc5BpEyFJMPf34J1mXuWdXjNcbA";
+        AppAccount account = AppAccount.loginByAccessToken(url, token);
+        User toUser = new User();
+        toUser.setLoginName("t13");
+        account.setFromUserLoginName("t11");
+        System.out.println(account.sendMessageToUser(toUser, "test"));
 
-		System.out.println(JSON.toJSONString(account.findUserByLoginname("liuwen")));
-//		List list = account.getAllDepartments();
-		System.out.println(1);
-	}
-	
+    }
+
 }
 
