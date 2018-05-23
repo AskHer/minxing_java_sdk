771f4148c686d8e8c6e5eedd97b17d09886cfdf8
suzz@dehuinet.com <0316llds>
2018-05-23 12:24:24 +0800

    待办事项示例修改

--- example/TestTasksApi.java
+++ example/TestTasksApi.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class TestTasksApi {
 
     public static void main(String args[]) throws Exception {
-        AppAccount appAcount = AppAccount.loginByAccessToken("http://192.168.40.19:7920", "suibianxieixie");
+        AppAccount appAcount = AppAccount.loginByAccessToken("http://example.com", "example_access_token");
         // 新建一个待办信息,参数依次为 标题,备注,用户ID,类别编码,系统来源名称,待办详情地址,提醒基准时间,提醒时间偏移量,公众号ID,公众号secret,是否需要立即提醒
         Task task = new Task("标题3", "备注6", 18, "12121212", "外部系统", "www.example.com", new Date(), new Integer[]{15}, "13131", "242424", false);
         // 调用接口新增待办事项,返回值为待办事项的ID
